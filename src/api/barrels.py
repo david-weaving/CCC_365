004e53c30d505db93d5780b271d18e14e3ac55d1
David Weaver <127688968+david-weaving@users.noreply.github.com>
2024-09-24 20:40:08 -0700

    Update barrels.py

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -76,7 +76,7 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
             result = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory"))
             green_ml = result.scalar()
                                         
-    for barrel in barrels_delivered:
+    for barrel in wholesale_catalog:
             # I want to purchase a barrel iff the barrel is green
         if barrel.potion_type[1] > 0: # making sure there is green ml in my barrel
             if barrel.price <= gold and green_pots < 10: # if i can afford it
