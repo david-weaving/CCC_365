03e6d4f438d44e92a3e8a3bd36cdd6872b53b887
David Weaver <127688968+david-weaving@users.noreply.github.com>
2024-09-24 19:23:42 -0700

    Update barrels.py

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -59,10 +59,10 @@ def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
             result = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory"))
             gold = result.scalar()
                 
-            result = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")
+            result = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory"))
             green_pots = result.scalar()
 
-            result = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory")
+            result = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory"))
             green_ml = result.scalar()
                                         
         for barrel in barrels_delivered:
@@ -72,7 +72,7 @@ def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
                     gold -= barrel.price # deduct price
                     green_ml += barrel.potion_type[1] # gain green_ml
         
-        connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET gold={gold}")
+        connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET gold={gold}"))
         connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_green_ml={green_ml}"))
 
     print(f"barrels delievered: {barrels_delivered} order_id: {order_id}")
