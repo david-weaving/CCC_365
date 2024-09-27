afbc93acbe607ebb2c89399f636906b659365168
David Weaver <127688968+david-weaving@users.noreply.github.com>
2024-09-26 17:02:13 -0700

    Update catalog.py

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -1,20 +1,25 @@
 from fastapi import APIRouter
+import sqlalchemy 
+from src import database as db
 
 router = APIRouter()
 
 
 @router.get("/catalog/", tags=["catalog"])
 def get_catalog():
-    """
-    Each unique item combination must have only a single price.
-    """
+
+    with db.engine.begin() as connection:
+        
+        result = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory"))
+        green_pots = result.scalar()
+
 
     return [
             {
-                "sku": "RED_POTION_0",
-                "name": "red potion",
-                "quantity": 1,
+                "sku": "GREEN_POTION_0",
+                "name": "green potion",
+                "quantity": green_pots,
                 "price": 50,
-                "potion_type": [100, 0, 0, 0],
+                "potion_type": [0, 100, 0, 0],
             }
         ]
