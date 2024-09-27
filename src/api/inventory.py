7822289b59f9880249d9cf3067523870c5016ac8
David Weaver <127688968+david-weaving@users.noreply.github.com>
2024-09-26 17:01:40 -0700

    Update inventory.py

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -2,6 +2,9 @@ from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from src.api import auth
 import math
+import sqlalchemy 
+from src import database as db
+
 
 router = APIRouter(
     prefix="/inventory",
@@ -12,8 +15,17 @@ router = APIRouter(
 @router.get("/audit")
 def get_inventory():
     """ """
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory"))
+        green_pots = result.scalar()
+        
+        result = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory"))
+        green_ml = result.scalar()
+
+        result = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory"))
+        gold = result.scalar()
     
-    return {"number_of_potions": 0, "ml_in_barrels": 0, "gold": 0}
+    return {"number_of_potions": green_pots, "ml_in_barrels": green_ml, "gold": gold}
 
 # Gets called once a day
 @router.post("/plan")
