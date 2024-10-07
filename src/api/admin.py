22f253eabd5c6e1ad819cad018be89c958e8b0d4
David Weaver <127688968+david-weaving@users.noreply.github.com>
2024-10-07 09:01:15 -0700

    Now resets DB when admin.py called

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, Request
 from pydantic import BaseModel
 from src.api import auth
+import sqlalchemy 
+from src import database as db
 
 router = APIRouter(
     prefix="/admin",
@@ -10,9 +12,14 @@ router = APIRouter(
 
 @router.post("/reset")
 def reset():
-    """
-    Reset the game state. Gold goes to 100, all potions are removed from
-    inventory, and all barrels are removed from inventory. Carts are all reset.
-    """
+
+
+    with db.engine.begin() as connection:
+
+        connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET gold=100,num_green_potions={0},num_blue_potions={0},num_green_ml={0},num_blue_ml={0},num_red_ml={0} WHERE id = {1}"))
+        connection.execute(sqlalchemy.text("DELETE FROM cart"))
+        connection.execute(sqlalchemy.text("ALTER SEQUENCE cart_id_seq RESTART WITH 1")) # start from 1 again
+
+
     return "OK"
 
