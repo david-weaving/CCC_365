ac02a1ab702da61371161851f099a985c33929ff
David Weaver <127688968+david-weaving@users.noreply.github.com>
2024-10-07 21:40:14 -0700

    fixed red potions to be set to 0 when admin.py called

--- src/api/admin.py
+++ src/api/admin.py
@@ -16,7 +16,7 @@ def reset():
 
     with db.engine.begin() as connection:
 
-        connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET gold=100,num_green_potions={0},num_blue_potions={0},num_green_ml={0},num_blue_ml={0},num_red_ml={0} WHERE id = {1}"))
+        connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET gold=100,num_green_potions={0},num_blue_potions={0},num_red_potions={0},num_green_ml={0},num_blue_ml={0},num_red_ml={0} WHERE id = {1}"))
         connection.execute(sqlalchemy.text("DELETE FROM cart"))
         connection.execute(sqlalchemy.text("ALTER SEQUENCE cart_id_seq RESTART WITH 1")) # start from 1 again
 
