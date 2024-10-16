cd71e176b39c2607fc2629cc62412298c4a59ce8
David Weaver <127688968+david-weaving@users.noreply.github.com>
2024-10-15 23:22:30 -0700

    Updated to match new db

--- src/api/admin.py
+++ src/api/admin.py
@@ -16,11 +16,9 @@ def reset():
 
     with db.engine.begin() as connection:
 
-        connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET gold=100,num_green_potions={0},num_blue_potions={0},num_red_potions={0},num_green_ml={0},num_blue_ml={0},num_red_ml={0} WHERE id = {1}"))
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold=100"))
         connection.execute(sqlalchemy.text("DELETE FROM cart"))
-        connection.execute(sqlalchemy.text("ALTER SEQUENCE cart_id_seq RESTART WITH 1")) # start from 1 again
-
-    print("SHOP HAS BEEN RESET")
+        connection.execute(sqlalchemy.text("UPDATE ml_inventory SET red_ml=0, green_ml=0, blue_ml=0, black_ml=0"))
 
 
     return "OK"
