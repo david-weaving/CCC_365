82a4da23c3fb7fa8e952cbd6f8893bf71af1a667
David Weaver <127688968+david-weaving@users.noreply.github.com>
2024-10-15 23:24:39 -0700

    db changes for new potions

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -4,61 +4,29 @@ from src import database as db
 
 router = APIRouter()
 
-
 @router.get("/catalog/", tags=["catalog"])
 def get_catalog():
 
     with db.engine.begin() as connection:
         
-        result = connection.execute(sqlalchemy.text("SELECT num_red_potions FROM global_inventory"))
-        red_pots = result.scalar()
-
-        result = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory"))
-        green_pots = result.scalar()
-
-        result = connection.execute(sqlalchemy.text("SELECT num_blue_potions FROM global_inventory"))
-        blue_pots = result.scalar()
-
-    
-    print(f"Catalog has the following Potions: Green: {green_pots}, Red: {red_pots}, Blue: {blue_pots} and price is 50 gold")
+        result = connection.execute(sqlalchemy.text("SELECT potions.id, COALESCE(sum(potion_ledgers.amount),0) AS inventory, potions.red_ml AS red, potions.green_ml AS green, potions.blue_ml AS blue, potions.dark_ml AS dark, potions.potion_name AS name FROM potions LEFT JOIN potion_ledgers ON potion_ledgers.potion_id = potions.id GROUP BY potions.id ORDER BY potions.id"))
+        catalog = result.fetchall()
 
     my_catalog = []
 
-    if red_pots > 0:
-
-        my_catalog.append(
+    for pot_id, amount, red, green, blue, dark, name in catalog:
+        print(f"ID: {pot_id}, amount: {amount}, potion: {name}")
+        if amount > 0:
+            my_catalog.append(
                         {
-                "sku": "RED_POTION",
-                "name": "red potion",
-                "quantity": red_pots,
+                "sku": name,
+                "name": name,
+                "quantity": int(amount),
                 "price": 50,
-                "potion_type": [100, 0, 0, 0],
+                "potion_type": [red, green, blue, dark],
             }
         )
 
-    if green_pots > 0:
-
-        my_catalog.append(
-                        {
-                "sku": "GREEN_POTION",
-                "name": "green potion",
-                "quantity": green_pots,
-                "price": 50,
-                "potion_type": [0, 100, 0, 0],
-            }
-        )
-
-    if blue_pots > 0:
-
-        my_catalog.append(
-                        {
-                "sku": "BLUE_POTION",
-                "name": "blue potion",
-                "quantity": blue_pots,
-                "price": 50,
-                "potion_type": [0, 0, 100, 0],
-            }
-        )
 
     
 
