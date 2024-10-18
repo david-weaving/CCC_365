b457809c9468a33bfda19e63ca80f78147103880
David Weaver <127688968+david-weaving@users.noreply.github.com>
2024-10-18 14:33:42 -0700

    make 5 of every potion

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -74,7 +74,7 @@ def get_bottle_plan():
             quantity = 0
             if sum((r1,b1,g1,d1)) > 0: # so we dont append only 0's
                 
-                while r1 >= r2 and g1 >= g2 and b1 >= b2 and d1 >=d2 and quantity < 10: # currently making 10 of every potion I can
+                while r1 >= r2 and g1 >= g2 and b1 >= b2 and d1 >=d2 and quantity < 5: # currently making 10 of every potion I can
                     r1 -= r2 # r1 represents my inventory, r2 is required potions
                     g1 -= g2
                     b1 -= b2
