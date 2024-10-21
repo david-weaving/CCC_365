5412e3222dbb2f726056eccb71f584eef0dfc38a
David Weaver <127688968+david-weaving@users.noreply.github.com>
2024-10-20 21:29:25 -0700

    deleting cart after cart_line_item

--- src/api/admin.py
+++ src/api/admin.py
@@ -17,9 +17,9 @@ def reset():
     with db.engine.begin() as connection:
 
         connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold=100"))
-        connection.execute(sqlalchemy.text("DELETE FROM cart"))
         connection.execute(sqlalchemy.text("UPDATE ml_inventory SET red_ml=0, green_ml=0, blue_ml=0, black_ml=0"))
         connection.execute(sqlalchemy.text("DELETE FROM cart_line_item"))
+        connection.execute(sqlalchemy.text("DELETE FROM cart"))
         connection.execute(sqlalchemy.text("DELETE FROM potion_ledgers"))
 
 
