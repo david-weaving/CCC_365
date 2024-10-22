a4399c7607ce2d3cef5f18e1a32ae744715b415a
David Weaver <127688968+david-weaving@users.noreply.github.com>
2024-10-22 10:55:55 -0700

    Update catalog.py

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -9,27 +9,27 @@ def get_catalog():
 
     with db.engine.begin() as connection:
         
-        result = connection.execute(sqlalchemy.text("SELECT potions.id, COALESCE(sum(potion_ledgers.amount),0) AS inventory, potions.red_ml AS red, potions.green_ml AS green, potions.blue_ml AS blue, potions.dark_ml AS dark, potions.potion_name AS name FROM potions LEFT JOIN potion_ledgers ON potion_ledgers.potion_id = potions.id GROUP BY potions.id ORDER BY potions.id"))
+        result = connection.execute(sqlalchemy.text("SELECT potions.id, COALESCE(sum(potion_ledgers.amount),0) AS inventory, potions.red_ml AS red, potions.green_ml AS green, potions.blue_ml AS blue, potions.dark_ml AS dark, potions.potion_name AS name, potions.price AS price FROM potions LEFT JOIN potion_ledgers ON potion_ledgers.potion_id = potions.id GROUP BY potions.id ORDER BY potions.id"))
         catalog = result.fetchall()
 
     my_catalog = []
 
-    for pot_id, amount, red, green, blue, dark, name in catalog:
-        print(f"ID: {pot_id}, amount: {amount}, potion: {name}")
+    for pot_id, amount, red, green, blue, dark, name, price in catalog:
+        
         if amount > 0:
-            
+            print(f"ID: {pot_id}, amount: {amount}, potion: {name}")
             
             my_catalog.append(
                         {
                 "sku": name,
                 "name": name.replace("_", " "),
                 "quantity": int(amount),
-                "price": 50,
+                "price": price,
                 "potion_type": [red, green, blue, dark],
             }
            
         )
-            print(my_catalog)
+
 
     
 
