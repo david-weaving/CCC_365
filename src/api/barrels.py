ccbd07161ebfc3ec1ebdb6d9df6b4014813ee60d
David Weaver <127688968+david-weaving@users.noreply.github.com>
2024-10-23 13:15:07 -0700

    buying bigger barrels

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -82,7 +82,7 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
 
     for barrel in wholesale_catalog:
         quantity = 0 # might change to an array for quantity of each potion type
-        if barrel.potion_type[0] == 1 and barrel.ml_per_barrel == 200 and barrel.price <= gold:
+        if barrel.potion_type[0] == 1 and barrel.ml_per_barrel == 500 and barrel.price <= gold:
 
             while barrel.price <= gold and quantity < 1 and barrel.quantity > 0:
                 quantity += 1
@@ -99,7 +99,7 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
         }
         )
         quantity=0
-        if barrel.potion_type[1] == 1 and barrel.ml_per_barrel == 200 and barrel.price <= gold:
+        if barrel.potion_type[1] == 1 and barrel.ml_per_barrel == 500 and barrel.price <= gold:
             
             while barrel.price <= gold and quantity < 1 and barrel.quantity > 0:
                 quantity += 1
@@ -117,7 +117,7 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
         }
         )
         quantity=0
-        if barrel.potion_type[2] == 1 and barrel.ml_per_barrel == 200 and barrel.price <= gold:
+        if barrel.potion_type[2] == 1 and barrel.ml_per_barrel == 500 and barrel.price <= gold:
      
             while barrel.price <= gold and quantity < 1 and barrel.quantity > 0:
                 quantity += 1
