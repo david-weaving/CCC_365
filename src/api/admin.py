2eabf9aff83c869399a2f3016fa00abaebb2b68a
David Weaver <127688968+david-weaving@users.noreply.github.com>
2024-11-01 22:22:10 -0700

    Ledgers included

--- src/api/admin.py
+++ src/api/admin.py
@@ -16,11 +16,19 @@ def reset():
 
     with db.engine.begin() as connection:
 
-        connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold=100"))
-        connection.execute(sqlalchemy.text("UPDATE ml_inventory SET red_ml=0, green_ml=0, blue_ml=0, black_ml=0"))
+        connection.execute(sqlalchemy.text("DELETE FROM gold_ledgers"))
+        connection.execute(sqlalchemy.text("INSERT INTO gold_ledgers (gold) VALUES (:gold)"),
+            {"gold": 100}
+            )
+        connection.execute(sqlalchemy.text("DELETE FROM ml_ledgers"))
+        connection.execute(sqlalchemy.text("INSERT INTO ml_ledgers (red_ml, green_ml, blue_ml, dark_ml) VALUES (:red_ml, :green_ml, :blue_ml, :dark_ml)"),
+                           {"red_ml": 0, "green_ml": 0, "blue_ml": 0, "dark_ml": 0})
         connection.execute(sqlalchemy.text("DELETE FROM cart_line_item"))
         connection.execute(sqlalchemy.text("DELETE FROM cart"))
         connection.execute(sqlalchemy.text("DELETE FROM potion_ledgers"))
+        connection.execute(sqlalchemy.text("UPDATE storage SET pots = :pots, ml = :ml WHERE id = :id"),
+    {"pots": 50, "ml": 10000, "id": 1}
+)
 
 
     return "OK"
