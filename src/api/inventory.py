db3b786d90a6d03562128cabb3e87cbc256a1e0b
David Weaver <127688968+david-weaving@users.noreply.github.com>
2024-11-03 16:50:00 -0800

    Fixed capacity buying plan to match ledgers table

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -38,7 +38,7 @@ def get_capacity_plan():
     """
 
     with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory"))
+        result = connection.execute(sqlalchemy.text("SELECT SUM(gold) FROM global_ledgers"))
         gold = result.scalar()
 
     if gold >= 2000:
